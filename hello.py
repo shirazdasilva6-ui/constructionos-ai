f3330f9b8ad8081a496cd16cffd4249fd7bc4eff
shirazdasilva6-ui <shirazdasilva6@gmail.com>
2026-09-01 23:05:16 +0200

    Add project_check.py with dictionary example

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, ConstructionOS AI!")
+project_name = "ConstructionOS AI"
+active_projects = 12
+budget_remaining = 45000.50
+is_over_budget = False
+
+print(project_name)
+print(active_projects)
+print(budget_remaining)
+print(is_over_budget)
