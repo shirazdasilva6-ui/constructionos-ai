3dd105eebf7c98b8bac0512a142d68e82bd6c9ee
shirazdasilva6-ui <shirazdasilva6@gmail.com>
2026-09-01 23:03:56 +0200

    Add dictionary-based project data example

---
+++ project_check.py
@@ -0,0 +1,9 @@
+project = {
+    "project_name": "ConstructionOS AI",
+    "active_projects": 12,
+    "budget_remaining": 45000.50,
+    "is_over_budget": False
+}
+
+print(project["project_name"])
+print(project["budget_remaining"])
